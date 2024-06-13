fc0a90c4eb278c7589f1c29c42a865ff7fc5f5ac
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-06-13 16:35:02 +0900

    footer, xmlns:th="http://www.thymeleaf.org", mapper

---
+++ testProject2/src/main/java/com/example/demo/DTO/MemberDTO.java
@@ -0,0 +1,18 @@
+package com.example.demo.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberDTO {
+	private String memberName;
+	private String memberPhone;
+	private String memberAge;
+}
