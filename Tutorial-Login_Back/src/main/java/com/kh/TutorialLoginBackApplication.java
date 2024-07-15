13edcf82df34ef59f3356d4897db7a954602bb31
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-07-15 17:44:43 +0900

    gradle todo

---
+++ Tutorial-Login_Back/src/main/java/com/kh/TutorialLoginBackApplication.java
@@ -0,0 +1,13 @@
+package com.kh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialLoginBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialLoginBackApplication.class, args);
+	}
+
+}
