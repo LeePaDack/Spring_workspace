9c30758b8f92dfa1f64cd8dbdfe263f07bf3e73e
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-07-16 11:52:28 +0900

    gradle react

---
+++ Tutorial-Login_Back/src/main/java/com/kh/controller/TodoController.java
@@ -0,0 +1,57 @@
+package com.kh.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kh.dto.TodoMember;
+import com.kh.service.TodoService;
+
+import lombok.extern.slf4j.Slf4j;
+
+// SpringBoot 어플리케이션(폴더 안)에서 HTML 코드를 작성해주는 것이 아니라
+// React 나 외부로 url 주소 공유할 때 사용
+
+@RestController
+@Slf4j
+public class TodoController {
+	@Autowired
+	private TodoService service;
+	
+	/** 아이디 중복검사
+	 * @param(파라미터 = 매개변수 = 중간에 개입해서 값을 변하게 하는 수)
+	 * @param id 값을 가져와서 아이디 중복 결과를 보여줄 것
+	 * @return 아이디 중복 확인 후에 결과를 되돌려 주는 것
+	 * @return 중복 : 1, 사용 가능 : 0
+	 */
+	
+	@GetMapping("idCheck")
+	public int idCheck(@RequestParam("id") String id) {
+		return service.idCheck(id);
+	}
+	
+	/** 회원가입
+	 * @param member 회원가입할 때 작성한 정보를 모두 가져오기
+	 * @return 회원가입을 성공하면 : 1  실패하면 : 0 을 줄 것
+	 * 
+	 * @Request      DB 에 특정 값이 있는지, 또는 값을 넣겠다, 값을 수정하겠다 요청
+	 * @RequestParam 특정 값민 선턱해서 요청, 특정 값을 변환해서 요청
+	 * @RequestBody  프론트에서 작성하고 Body 로 전달해주는 모든 값을 넣어주겠다.
+	 * 				 우리가 사용하는 React 기준 
+	 * 				 body : JSON.stringify(input값들)
+	 * 			 	 input값들 에 DB 로 넣어주겠다는 모든 값을 DB 에 넣겠다 선언
+	 * 
+	 * @Response       DB 에서 전달받은 값을 프론트에 전달할 때 사용
+	 * @ResponseParam  특정 값만 프론트에 전달
+	 * @ResponseBody   HTML 에서 BODY 보여줄 모든 값을 전달
+	 */
+	@PostMapping(value="/signup")
+	public int signup(@RequestBody TodoMember member) {
+		return service.signup(member);
+	}
+	
+	
+}
--- Tutorial-Login_Back/src/main/java/com/kh/service/TodoServiceImpl.java
+++ Tutorial-Login_Back/src/main/java/com/kh/service/TodoServiceImpl.java
@@ -3,10 +3,36 @@ package com.kh.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kh.dto.TodoMember;
 import com.kh.mapper.TodoMapper;
+/*
+Service ServiceImpl
+Service = 코드 작성하는데 있어 이런 서비스를 사용하겠다는 계약 정의
+어떤 메서드들(기능들)이 있어야하는지, 그 메서드들(기능들)이 어떤 역할을 하는지 정의
+ServiceImpl = 인터페이스로 작성한 Service 의 실제 기능을 작성
+비슷한 기능을 각 서비스에 맞춰서 사용하겠다는 의미
+
+Ex) createUser = user 를 만들 것임
+user 를 만드는 도중 관리자 - 소비자를 나눠서 만들 것
+ */
+
 
 @Service
 public class TodoServiceImpl implements TodoService{
 	@Autowired
 	private TodoMapper mapper;
+	
+	
+	// interface 에 기능 명시가 되어있고
+	// 명시된 기능을 완벽하게 만든다는 @Override = 재사용 이라는 표기를 작성
+	// 개발자간의 약속, 생략 가능하긴하지만 작성해주는 것이 좋음
+	@Override
+	public int idCheck(String id) {
+		return mapper.idCheck(id);
+	}
+	
+	@Override
+	public int signup(TodoMember member) {
+		return mapper.signup(member);
+	}
 }
